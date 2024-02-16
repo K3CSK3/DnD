d14a0e4846d6688391d3fc30b4fc6a7fc84d22fa
K3CSK3 <wiattila8@gmail.com>
2024-02-16 22:51:03 +0100

    2024. 02. 16. - MŰKSZIK A MAUI

---
+++ MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        double progressCount = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SetProgress(object sender, EventArgs e) {
+            SliderProgress.Progress = (double)Slide.Value/Slide.Maximum;
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            progressCount++;
+
+            /*
+            count = 0;
+            progressCount = 0;
+            */
+
+            Progress.Progress = progressCount / 100;
+
+            if (progressCount > 100) {
+                progressCount = 0;
+            }
+            
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
